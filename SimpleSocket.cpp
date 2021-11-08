03b1fd1c0ee44d7343e99b6f36e041720ee21d34
adriabama06 <adriabama06@gmail.com>
2021-11-08 17:44:00 +0100

    add some things

--- SimpleSocket.cpp
+++ SimpleSocket.cpp
@@ -1,12 +1,45 @@
 #include "SimpleSocket.hpp";
 
+// Constructor
+
 HDE::SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port, u_long interf)
 {
 	// Define addres structure
 	address.sin_family = domain;
 	address.sin_port = htons(port);
-	address.sin_addr.S_un = htonl(interf);
-	// Establish connetion
+	address.sin_addr.s_addr = htonl(interf);
+	// Establish socket
 	sock = socket(domain, service, protocol);
-	// Bind
+	test_connection(sock);
+	// establish network connetion
+	connection = connect_to_network(sock, address);
+	test_connection(connection);
+}
+
+// Test connection virtual function
+
+void HDE::SimpleSocket::test_connection(int item_to_test)
+{
+	// Confirm connection or socket has ben established
+	if(item_to_test < 0) {
+		perror("Error: Fail on connect");
+		exit(EXIT_FAILURE);
+	}
+}
+
+// Get functions
+
+struct sockaddr_in HDE::SimpleSocket::get_address()
+{
+	return address;
+}
+
+int HDE::SimpleSocket::get_sock()
+{
+	return sock;
+}
+
+int HDE::SimpleSocket::get_connection()
+{
+	return connection;
 }
--- SimpleSocket.hpp
+++ SimpleSocket.hpp
@@ -4,6 +4,7 @@
 //#include <sys/socket.h> // change to this if you not are on windows
 //#include <windows.h>
 //#include <netinet/in.h> // change to this if you not are on windows
+#include <iostream>
 
 namespace HDE
 {
@@ -12,8 +13,17 @@ namespace HDE
 		private:
 			struct sockaddr_in address;
 			int sock;
+			int connection;
 		public:
 			SimpleSocket(int domain, int service, int protocol, int port, u_long interf);
+			// Virtual function to connect to the network
+			virtual int connect_to_network(int sock, struct sockaddr_in adress) = 0;
+			// Function to check if the socket or connection
+			void test_connection(int);
+			// Get functions
+			struct sockaddr_in get_address();
+			int get_sock();
+			int get_connection();
 		};
 }
 
